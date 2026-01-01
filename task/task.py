9106787ab1a45a68b6296444487ace622b6759a8
Reniel James <138281617+RenielJ@users.noreply.github.com>
2026-01-01 23:42:33 +0800

    Day 1: Band Name Generator Project
    The Common functions and the basic skill of the python which is variable for inputting or storing a piece of data.

---
+++ task/task.py
@@ -0,0 +1,10 @@
+# Greetings of the program
+print("Welcome the Band name program")
+
+# variable to prompt the city
+city = input("What's the name of the city you grew up in? \n")
+
+# variable to prompt the pet
+pet = input("What is the name of your pet? \n")
+
+print("Your Band Name could be: " + city + " " + pet)
