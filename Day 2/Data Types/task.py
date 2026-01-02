1083d686e633faf4cddc1141ab3c898adda3f97c
Reniel James <138281617+RenielJ@users.noreply.github.com>
2026-01-02 19:38:21 +0800

    Day 2: Tip Calculator Project
    Practice python code: Understanding Data types and how to manipulate strings

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,18 @@
+# Subscripting
+print("Hello" [-2])
+
+# String
+print("123" + "345") # Concatenated
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_345_456) # 123,345,567 interpreted in Human but in computer used "_"
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,33 @@
+# Manipulation of decimal point
+print(int(3.738492))
+print(round(3.738492, 0))
+print(round(3.14159, 0))
+print(round(3.14159, 2))
+
+# Score of a users
+score = 2
+score += 2
+print(score)
+
+# f-strings
+age = input("What is your age?")
+print(f"You are {age} years old.")
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score}, your height is = {height}, You are winning is {is_winning}")
+
+
+# First option
+name = input("What is your name?")
+print(f"Hello {name}!")
+
+# Second option
+name = input("What is your name?")
+print("Hello " + name)
+
+# Third option
+age = 12
+print(f"You are {age} years old.")
